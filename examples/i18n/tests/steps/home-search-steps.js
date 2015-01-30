73ae7b9d6bf6ab4320bb35837b27f713692026c2
Danny Hall <rvfdan@gmail.com>
2015-01-30 14:52:04 +0000

    Updated package versions & French example steps

--- examples/i18n/tests/steps/home-search-steps.js
+++ examples/i18n/tests/steps/home-search-steps.js
@@ -3,16 +3,16 @@ var searchResults = require('../pages/search-results');
 
 exports.define = function (steps) {
 
-  steps.given("la page d'accueil affichée", function () {
+  steps.etantdonne("la page d'accueil affichée", function () {
     homePage.visit();
   });
 
-  steps.when("je cherche '$query'", function (query) {
+  steps.quand("je cherche '$query'", function (query) {
     homePage.txtSearch.sendKeys(query);
     homePage.btnSearch.click();
   });
 
-  steps.then("je dois voir '$heading' dans l'en-tête", function (heading) {
+  steps.alors("je dois voir '$heading' dans l'en-tête", function (heading) {
     searchResults.heading.getText().then(function (text) {
       text.should.equal(heading);
     });
